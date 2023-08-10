bcda951f883b6ea0119cd151183fcd61675da6fa
EucTech <euccreative@gmail.com>
2023-08-10 14:21:08 +0000

    This is a test case for *agrs **kwargs

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,7 @@
 import unittest
 import uuid
 from datetime import datetime
+from datetime import timedelta
 from models.base_model import BaseModel
 
 
@@ -42,3 +43,23 @@ class TestBaseModel(unittest.TestCase):
             '__class__': self.b.__class__.__name__
         }
         self.assertEqual(self.b.to_dict(), value)
+
+    def test_kwargs(self):
+        """To test for **kwargs"""
+        b2 = BaseModel()
+
+        dictionary = {
+            'created_at': b2.created_at.isoformat(),
+            'updated_at': b2.updated_at.isoformat(),
+            '__class__': b2.__class__.__name__
+        }
+
+        new_dic = BaseModel(**dictionary)
+
+        for key, value in dictionary.items():
+            if key == 'created_at' or key == 'updated_at':
+                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                differ = timedelta(microseconds=999999)
+                setattr(self, key, value)
+                self.assertTrue(hasattr(new_dic, key))
+                self.assertTrue(abs(getattr(new_dic, key) - value) < differ)
