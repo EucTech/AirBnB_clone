2984d2f9ba69349f7d2f5bb6407a0bf1380ffdc8
EucTech <euccreative@gmail.com>
2023-08-11 18:03:25 +0000

    fixed erroron reload and save function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,7 @@ and deserializes JSON file to instances
 
 import json
 import os
+from models.base_model import BaseModel
 
 class FileStorage:
     """defining a class FileStorage"""
@@ -16,26 +17,33 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """function that adds a new instance to the __objects
+            dictionary"""
         obj_class_name = obj.__class__.__name__
-        obj_id = "{}.{}".format(obj_class_name, obj.id)
-        FileStorage.__objects[obj_id] = obj
+        key = "{}.{}".format(obj_class_name, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as file1:
+        with open(self.__file_path, "w", encoding="utf-8") as file1:
             obj_dict = {key: value.to_dict()
-                    for key, value in FileStorage.__objects.items()}
+                    for key, value in self.__objects.items()}
             json.dump(obj_dict, file1)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as file1:
-            obj_dict = json.load(file1)
-            FileStorage.__objects = obj_dict
+        if os.path.exists(self.__file_path):
+            try:
+                with open(self.__file_path, "r", encoding="utf-8") as file1:
+                    obj_dict = json.load(file1)
 
+                for key, value in obj_dict.items():
+                    class_name = value['__class__']
+                    cls = eval(class_name)
+                    class_instance = cls(**value)
+                    self.__objects[key] = class_instance
+            except Exception as e:
+                    pass
