c90d24b0770e267227c3cb004f50d980c952f714
EucTech <euccreative@gmail.com>
2023-08-14 22:53:25 +0000

    fixed code

--- console.py
+++ console.py
@@ -55,7 +55,7 @@ class HBNBCommand(cmd.Cmd):
         Prints the string representation of an
         instance based on the class name and id
         """
-        if args is None:
+        if len(args) < 1:
             print("** class name missing **")
             return
         else:
